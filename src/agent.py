e34aab0903b20f65f25a5350643731d9716bdd64
anderzzz <ohrn.anders@gmail.com>
2024-07-06 15:57:56 +0200

    Create slimmed down agent that uses tools and returns tool output only

--- src/agent.py
+++ src/agent.py
@@ -3,6 +3,7 @@
 """
 import json
 from typing import Optional, Sequence
+from abc import ABC, abstractmethod
 
 from anthropic.types import (
     Message,
@@ -19,8 +20,8 @@ from semantics import get_anthropic_client, send_request_to_anthropic_message_cr
 from tools import get_tool
 
 
-class Agent:
-    """The agent that implements a semantic process
+class Agent(ABC):
+    """Base class for agents
 
     """
     def __init__(self,
@@ -29,36 +30,55 @@ class Agent:
                  model: str = 'claude-3-haiku-20240307',
                  temperature: float = 1.0,
                  max_tokens: int = 4096,
-                 tools: Optional[Sequence[str]] = None,
-                 tool_choice: Optional[str] = 'auto',
                  ):
         self.anthropic_client = get_anthropic_client(api_key)
         self.system_instruction = instruction
         self.model = model
         self.temperature = temperature
         self.max_tokens = max_tokens
-        self.tool_choice = {'type': tool_choice}
+
+    @abstractmethod
+    def run(self, text: str):
+        pass
+
+
+class AgentToolInvokeReturn(Agent):
+    """The agent that implements a semantic process
+
+    """
+    def __init__(self,
+                 instruction: str,
+                 api_key: str,
+                 tools: Sequence[str],
+                 model: str = 'claude-3-haiku-20240307',
+                 temperature: float = 1.0,
+                 max_tokens: int = 4096,
+                 ):
+        super().__init__(
+            instruction=instruction,
+            api_key=api_key,
+            model=model,
+            temperature=temperature,
+            max_tokens=max_tokens,
+        )
+        self.tool_choice = {'type': 'any'}
         self.tools = []
-        if tools is not None:
-            with open(CONF_CV_DATA) as f:
-                cv_data = json.load(f)
-            with open(CONF_FUNCS) as f:
-                funcs = json.load(f)
-            for tool in tools:
-                if tool in cv_data:
-                    self.tools.append(cv_data[tool])
-                elif tool in funcs:
-                    self.tools.append(funcs[tool])
-                else:
-                    raise ValueError(f'Tool "{tool}" not found in CV data or functions')
+        with open(CONF_CV_DATA) as f:
+            cv_data = json.load(f)
+        with open(CONF_FUNCS) as f:
+            funcs = json.load(f)
+        for tool in tools:
+            if tool in cv_data:
+                self.tools.append(cv_data[tool])
+            elif tool in funcs:
+                self.tools.append(funcs[tool])
+            else:
+                raise ValueError(f'Tool "{tool}" not found in CV data or functions')
 
     def run(self, text: str):
         """Bla bla
 
         """
-        _tool_use = False
-        _content_obj = None
-
         messages = [
             MessageParam(
                 content=text,
@@ -75,34 +95,19 @@ class Agent:
             max_tokens=self.max_tokens,
             temperature=self.temperature,
         )
+        tool_outs = {}
         for response_message in response.content:
-            print (response_message)
-            if isinstance(response_message, TextBlock):
-                _content_obj = TextBlockParam(**response_message.model_dump())
-            elif isinstance(response_message, ToolUseBlock):
-                _tool_use = True
-                _content_obj = ToolUseBlockParam(**response_message.model_dump())
-            else:
-                raise ValueError(f'Unknown response type: {response_message.__class__.__name__}')
+            assert isinstance(response_message, ToolUseBlock)
 
-            messages.append(
-                MessageParam(
-                    content=[_content_obj],
-                    role='assistant',
-                )
-            )
+            tool_name = response_message.name
+            func_kwargs = response_message.input
+            tool_id = response_message.id
 
-            if _tool_use:
-                tool_name = response_message.name
-                func_kwargs = response_message.input
-                tool_id = response_message.id
+            tool = get_tool(tool_name)
+            try:
+                tool_return = tool(**func_kwargs)
+            except Exception as e:
+                raise RuntimeError(f'Error in tool "{tool_name}" with ID "{tool_id}": {e}')
+            tool_outs[tool_id] = tool_return
 
-                tool = get_tool(tool_name)
-                try:
-                    tool_return = tool(**func_kwargs)
-                except Exception as e:
-                    raise RuntimeError(f'Error in tool "{tool_name}" with ID "{tool_id}": {e}')
-
-                print (tool_return)
-
-        return 'hello'
+        return tool_outs
--- src/flow.py
+++ src/flow.py
@@ -3,14 +3,14 @@
 """
 import json
 
-from agent import Agent
+from agent import AgentToolInvokeReturn
 from drl import get_data
 
 
 def main():
     with open(get_data('education')) as f:
         education_data = json.load(f)
-    agent = Agent(
+    agent = AgentToolInvokeReturn(
         api_key='ANTHROPIC_API_KEY',
         instruction="""You assist the user in writing a resume. The text needs to be simplified and made more concise and less technical.
         
@@ -18,7 +18,6 @@ def main():
         Also note that the JSON data may contain references to `M3`, which should be replaced with the user's name, as provided by the user.
         """,
         tools=['education_university'],
-        tool_choice='any',
     )
     text = 'The following JSON data contains the education information of "Anders Ohrn".\n\n' + json.dumps(education_data, indent=4)
     out = agent.run(text)
