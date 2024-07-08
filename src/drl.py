63b863402b206dad9935a84c77821c852a7d7aae
anderzzz <ohrn.anders@gmail.com>
2024-07-08 13:01:51 +0200

    New synthetic data for George Samsa

--- src/drl.py
+++ src/drl.py
@@ -6,16 +6,22 @@ import os
 path_to_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../abtM3DataStore')
 
 _registry = {
-    'education': os.path.join(path_to_data_dir, 'education.json'),
-    'employment': os.path.join(path_to_data_dir, 'employment.json'),
+    'george sama': {
+        'education': os.path.join(path_to_data_dir, 'george_samsa_education.json'),
+        'employment': os.path.join(path_to_data_dir, 'george_samsa_employment.json'),
+    },
 }
 
 
-def get_data(name):
-    """Get data by name
+def get_data(person_name: str, data_type: str):
+    """Get data for person by name and type of personal data.
 
     """
     try:
-        return _registry[name]
+        person_bio = _registry[person_name]
+        try:
+            return person_bio[data_type]
+        except KeyError:
+            raise ValueError(f'Data type `"{data_type}"` not found for person `"{person_name}"`')
     except KeyError:
-        raise ValueError(f'Data "{name}" not found in registry')
+        raise ValueError(f'Data for person `"{person_name}"` not found in registry')
