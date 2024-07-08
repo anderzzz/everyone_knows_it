21560ed927fc7c71d308e38d77068c76b1512b6b
anderzzz <ohrn.anders@gmail.com>
2024-07-08 17:30:43 +0200

    Modest reorg of HTML templates

--- src/html_maker.py
+++ src/html_maker.py
@@ -1,8 +1,8 @@
-"""Creation of HTML files based on templates and data
+"""Creation of HTML files based on cv_templates and data
 
 """
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('templates'))
+env = Environment(loader=FileSystemLoader('cv_templates'))
 template = env.get_template('test_template.html')
 
