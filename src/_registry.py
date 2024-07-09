8e5dfcbd535e2e5ac86ef20846f67b1f644f3a97
anderzzz <ohrn.anders@gmail.com>
2024-07-09 10:26:44 +0200

    George to Gregor rename in synthetic data

--- src/_registry.py
+++ src/_registry.py
@@ -9,10 +9,10 @@ path_to_ad_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../jo
 
 _registry_persons = {
     'george samsa': {
-        'education': os.path.join(path_to_data_dir, 'george_samsa_education.json'),
-        'employment': os.path.join(path_to_data_dir, 'george_samsa_employment.json'),
-        'skills': os.path.join(path_to_data_dir, 'george_samsa_skills.json'),
-        'publications': os.path.join(path_to_data_dir, 'george_samsa_publications.json')
+        'education': os.path.join(path_to_data_dir, 'gregor_samsa_education.json'),
+        'employment': os.path.join(path_to_data_dir, 'gregor_samsa_employment.json'),
+        'skills': os.path.join(path_to_data_dir, 'gregor_samsa_skills.json'),
+        'publications': os.path.join(path_to_data_dir, 'gregor_samsa_publications.json')
     },
 }
 _registry_job_ads = {
