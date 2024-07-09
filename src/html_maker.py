40c334af137a99215f43865737ee288ba2a7b924
anderzzz <ohrn.anders@gmail.com>
2024-07-09 12:38:48 +0200

    Rename the template store

--- src/html_maker.py
+++ src/html_maker.py
@@ -1,8 +1,8 @@
-"""Creation of HTML files based on cv_templates and data
+"""Creation of HTML files based on formTemplateStore and data
 
 """
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('cv_templates'))
+env = Environment(loader=FileSystemLoader('formTemplateStore'))
 template = env.get_template('test_template.html')
 
